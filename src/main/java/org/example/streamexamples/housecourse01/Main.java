78e8c234279e7e7b2418dd43291c5cfb258129fb
Andreea Hincu <andreeahincu57@gmail.com>
2024-01-14 15:34:45 +0200

    14 ian

--- src/main/java/org/example/streamexamples/housecourse01/Main.java
+++ src/main/java/org/example/streamexamples/housecourse01/Main.java
@@ -3,6 +3,12 @@ package org.example.streamexamples.housecourse01;
 public class Main {
 
     public static void main(String[] args){
-        Couch couch1 = new Couch("Ador Mobila", "gray", );
+        Couch couch1 = new Couch("Ador Mobila", "gray", 2.3 );
+//        System.out.println(couch1.getBrand());
+//        System.out.println(couch1.getColor());
+//        System.out.println(couch1.getSize());
+        couch1.cleanCouch();
+        couch1.foldOutBed();
+        couch1.toString();
     }
 }
