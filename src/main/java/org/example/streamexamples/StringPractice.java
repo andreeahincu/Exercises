84973b61bd97ab800075fc1df496b2b6700a2e88
andreea.hincu <andreeahincu57@gmail.com>
2024-01-16 21:26:16 +0200

    String practice

---
+++ src/main/java/org/example/streamexamples/StringPractice.java
@@ -0,0 +1,26 @@
+package org.example.streamexamples;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+
+        // From w3resource
+        // 1. Write a Java program to get the character at the given index within the string.
+        String s1 = "Andreea";
+        char givenCharacter0= s1.charAt(0);
+        char givenCharacter1= s1.charAt(1);
+        char givenCharacter2= s1.charAt(2);
+        char givenCharacter3= s1.charAt(3);
+        char givenCharacter4= s1.charAt(4);
+        char givenCharacter5= s1.charAt(5);
+        char givenCharacter6= s1.charAt(6);
+        System.out.println(givenCharacter0);
+        System.out.println(givenCharacter1);
+        System.out.println(givenCharacter2);
+        System.out.println(givenCharacter3);
+        System.out.println(givenCharacter4);
+        System.out.println(givenCharacter5);
+        System.out.println(givenCharacter6);
+
+    }
+}
